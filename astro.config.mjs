825a9038964873fcf52feeb176169828e2b3cca4
Zachary Popowitz <popowitzzm@gmail.com>
2026-05-03 13:29:16 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://vifi.health",
+  site: "https://www.vifi.health",
   integrations: [tailwind()],
 });
